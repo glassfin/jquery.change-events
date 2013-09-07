67376ce8947ae394e82e5276122e48ad7eaf451e
Knight Fu <knight.w.fu@gmail.com>
2013-09-07 00:35:46 -0400

    some IE, Webkit, and Gecko original methods

--- src/contentchange.js
+++ src/contentchange.js
@@ -21,24 +21,56 @@
  */
 (function( $ )
 {
-   var ChangeEvent = $.CustomEvent.EventType( 'contentchange',
-   {
-      setup : function()
-      {
-      },
+   // define a list of names that
+   var nativeMethods = {
+      names : [ 'appendChild', 
 
+                'insertAdjacentElement', 
+
+                'insertAdjacentHTML',
+
+                'insertAdjacentText',
+
+                'insertBefore',
+
+                'removeChild',
+
+                'replaceChild'
+              ],
+      
+      // HTMLElement
+      base : {
+         appendChild : HTMLElement
+      }
+   },
+
+   ChangeEvent = $.CustomEvent.EventType( 'contentchange',
+   {
+
+      // get a handler in there that is called when the user adds
+      // another event handler
       process : function( data )
       {
-         //Object. data.target
-         console.log( Object.isExtensible( data.target ) );
+         if( typeof Object.defineProperty === 'function' )
+         {
+            var targetElement = data.target,
+
+                clone = targetElement.cloneNode();
+
+            targetElement.defineProperty;
+
+         }
+         else
+         {
+            // the best we can hope for is to leave innerHTML
+            // behind, and go for the other stuff: $.fn.html,
+            // and the remainder of those functions
+         }
       },
 
       cleanup : function( data )
       {
-      },
 
-      teardown : function()
-      {
       }
    });
 
@@ -52,3 +84,55 @@
       html : $.html
    }
 })( $ );
+
+
+/* IE prototypes for element that we should pay attention to
+
+LOG: currentStyle 
+LOG: runtimeStyle 
+LOG: className 
+LOG: contentEditable 
+LOG: dir                        // reading direction
+LOG: disabled 
+LOG: id 
+LOG: innerHTML 
+LOG: isContentEditable 
+LOG: outerHTML 
+LOG: style 
+LOG: title (not sure what the heck this does)
+LOG: document 
+LOG: filters 
+LOG: innerText 
+LOG: onbeforepaste 
+LOG: onfilterchange 
+LOG: outerText 
+LOG: parentTextEdit 
+LOG: insertAdjacentHTML 
+LOG: addFilter 
+LOG: applyElement 
+LOG: clearAttributes 
+LOG: dragDrop 
+LOG: insertAdjacentElement 
+LOG: insertAdjacentText 
+LOG: mergeAttributes 
+LOG: removeBehavior 
+LOG: removeFilter 
+LOG: replaceAdjacentText 
+LOG: setActive 
+LOG: removeNode 
+LOG: replaceNode 
+LOG: swapNode 
+LOG: removeAttribute 
+LOG: removeAttributeNS 
+LOG: removeAttributeNode 
+LOG: setAttribute 
+LOG: setAttributeNS 
+LOG: setAttributeNode 
+LOG: setAttributeNodeNS 
+LOG: textContent 
+LOG: appendChild 
+LOG: insertBefore 
+LOG: normalize 
+LOG: removeChild 
+LOG: replaceChild 
+*/
