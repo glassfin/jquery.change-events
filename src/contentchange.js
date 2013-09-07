c9b832bf9bb76808b029e27363e197364906d0d9
Knight Fu <knight.w.fu@gmail.com>
2013-09-06 22:43:20 -0400

    added some comments, and we're ready to roll

--- src/contentchange.js
+++ src/contentchange.js
@@ -1,23 +1,26 @@
+/**
+ * A jQuery plugin for detecting when the content of a DOM element
+ * has changed.
+ *
+ * Authored by : Knight W. Fu
+ * Copyright : I don't care, do whatever you will with it. If you
+ * want, claim it for your own.
+ *
+ * Minimum Version ( uses Object.defineProperty on DOM elements )
+ * 
+ * Ff : 4 ( Gecko 2 )
+ * Ch : 5
+ * IE : 9 ( 8 untested, but might work )
+ * Op : 11.6
+ * Sa : 5.1
+ *
+ * Note: this plugin wraps the native innerHTML,
+ * appendChild, removeChild, insertAdjacentElement, insertAdjacentHTML,
+ * insertAdjacentText, insertBefore, replaceChild ( Gecko )
+ *
+ */
 (function( $ )
 {
-   // this is for overriding jQuery functions
-   var overriders = {
-      jQGetterSetter : function( fnName, getterCondition ) 
-      {
-         var $fn = $.fn[fnName];
-         
-         $.fn[ fnName ] = function()
-         {
-            if( getterCondition?
-                  getterCondition.apply( this, arguments ) :
-                  arguments.length == 0 )
-               return $fn.apply( this, arguments );
-
-            // trigger the event
-         }
-      }
-   }
-
    var ChangeEvent = $.CustomEvent.EventType( 'contentchange',
    {
       setup : function()
@@ -26,6 +29,8 @@
 
       process : function( data )
       {
+         //Object. data.target
+         console.log( Object.isExtensible( data.target ) );
       },
 
       cleanup : function( data )
